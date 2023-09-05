40f1b14edaae0dd84124baff6720ac835ef03ae8
Matthew Kosarek <matthew@matthewkosarek.xyz>
2023-09-05 14:17:47 -0400

    Support for gbm

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 CXX=gcc
-CXXFLAGS=-g -Wall -pedantic `pkg-config --cflags libdrm`
+CXXFLAGS=-g -Wall -pedantic `pkg-config --cflags libdrm` `pkg-config --cflags gbm`
 src = $(wildcard *.c)
 obj = $(src:.c=.o)
 OUTPUT_PATH=.
 
-LDFLAGS=`pkg-config --libs libdrm`
+LDFLAGS=`pkg-config --libs libdrm` `pkg-config --libs gbm`
 
 drm_screen_shot: $(obj)
 	$(CXX) $(obj) -o "$(OUTPUT_PATH)/$@" $(LDFLAGS)
--- main.c
+++ main.c
@@ -10,6 +10,8 @@
 #include <drm/drm_fourcc.h>
 #include "bitmap.h"
 #include <string.h>
+#include <gbm.h>
+#include <errno.h>
 
 #define log_info(...) printf(__VA_ARGS__)
 #define log_error(...) fprintf(stderr, __VA_ARGS__)
@@ -25,7 +27,7 @@ int main(int argc, char **argv)
     struct _drmModeFB2 *fb;
     uint32_t buffer_id = 0;
 
-    int drm_fd = open("/dev/dri/card0", O_RDWR);
+    int drm_fd = open("/dev/dri/card1", O_RDWR);
     if (drm_fd < 0) {
         log_error("Unable to open DRI device\n");
         exit(1);
@@ -78,6 +80,8 @@ int main(int argc, char **argv)
     }
             
 
+    struct gbm_device* device = gbm_create_device(drm_fd);
+
     /* Output the first image that we find. */
     for (int handle_index = 0; handle_index < 4 && fb->handles[handle_index]; handle_index++) {
         int handle_fd;
@@ -88,17 +92,46 @@ int main(int argc, char **argv)
         }
 
         log_info("File Descriptor for the handle is: %d\n", handle_fd);
-
+        
         // Establish a memory map.
-        size_t size = fb->height * fb->pitches[handle_index];
-        const char *map = mmap(0, size, PROT_READ, MAP_SHARED, handle_fd, fb->offsets[handle_index]);
+        struct gbm_import_fd_data import_data = {
+            handle_fd,
+            fb->width,
+            fb->height,
+            fb->offsets[handle_index],
+            fb->pixel_format};
+        struct gbm_bo* bo = gbm_bo_import(
+            device,
+            GBM_BO_IMPORT_FD,
+            &import_data,
+            0);
 
-        log_info("Mapped memory successfully\n");
-
-        if (fb->pixel_format != DRM_FORMAT_XRGB8888) {
-            log_error("Unsupported pixel format\n");
-            exit(1);
+        if (!bo)
+        {
+            log_error("gbm_bo_import failed: %d", errno);
+            continue;
         }
+
+        void* map_data;
+        uint32_t map_stride;
+        void* void_map = gbm_bo_map(
+            bo,
+            0,
+            0,
+            fb->width,
+            fb->height,
+            0,
+            &map_stride,
+            &map_data);
+
+        if (void_map == MAP_FAILED)
+        {
+            gbm_bo_destroy(bo);
+            log_error("gbm_bo_map failed: %d", errno);
+            continue;
+        }
+
+        unsigned char* map = (unsigned char*)void_map;
 
         // Initialize the array of pixels
         unsigned char *img = NULL;
@@ -107,6 +140,7 @@ int main(int argc, char **argv)
         memset(img, 0, img_size);
 
         // Iterate over the framebuffer and add the pixels to the array.
+        uint32_t size = map_stride * fb->height;
         uint32_t offset = 0;
         for (uint32_t y = 0; y < fb->height; y++) {
             uint32_t read_in_row = 0;
@@ -131,7 +165,8 @@ int main(int argc, char **argv)
 
         generateBitmapImage(img, fb->height, fb->width, (char*)"output.bmp");
         free(img);
-        munmap(img, img_size);
+        gbm_bo_unmap(bo, map_data);
+        gbm_bo_destroy(bo);
     }
 
     return 0;
